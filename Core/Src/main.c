fecd787516281786094c915556bcef5a420351e5
Abisake Villalan <abisake123@gmail.com>
2026-04-09 00:54:17 +0530

    Using PLL clock with 180 Mhz

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,48 +20,118 @@
 #include "main.h"
 #include "stm32f4xx.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+// Systick
+static volatile uint32_t ms_ticks = 0;
 
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
-/* USER CODE BEGIN PFP */
-/* A UART function send one msg
-*/
-void uart_putchar(char msg)
+void SysTick_Handler(void)
 {
-	// Poll TXE bit (bit 7 of SR) — RM0390 page 812
-	while (!(USART2->SR & (1U << 7)));
-	USART2->DR = msg; // RM0390 page 814
+	ms_ticks++; // will be fired every 1 ms
 }
-/* USER CODE END PFP */
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
 
-/* USER CODE END 0 */
+void delay_ms(uint32_t ms)
+{
+	uint32_t start = ms_ticks;
+	while ((ms_ticks - start) < ms);
+}
+
+// Configure the clock for 180 MHz
+void clock_init(void)
+{
+	/* Flash wait states should be configured first -> FLASH_ACR, sEC 3.8.1, pAGE 80
+	 * And for 180 MHz, Table 5, 5 ws is required
+	 * Eanble prefetch - bit 8
+	 * Instr. cache - bit 9
+	 * Data cache - bit 10
+	 * */
+	FLASH->ACR = FLASH_ACR_LATENCY_5WS
+				| FLASH_ACR_PRFTEN
+				| FLASH_ACR_ICEN
+				| FLASH_ACR_DCEN;
+
+	// check it
+	while((FLASH->ACR & FLASH_ACR_LATENCY) != FLASH_ACR_LATENCY_5WS);
+
+	/* Configure PLL (RCC->PLLCFGR, page 128 & 129 Sec. 6.3.2
+	* M=8, N=180, P=2, Q=4, will convert 16 MHz to 180 MHz
+	* fVCO = 16/8 * 180 = 360 MHz  →  SYSCLK = 360/2 = 180 MHz
+	* fUSB = 360/4 = 90 MHz (USB needs 48 MHz, will need separate PLLSAI)
+	*/
+	RCC->PLLCFGR = (8U << 0) // PLLM = 8   — bits[5:0]
+                         | (180U <<  6)   // PLLN = 180 — bits[14:6]
+                         | (0U   << 16)   // PLLP = 2   — bits[17:16]: 00=÷2
+                         | (0U   << 22)   // PLLSRC=HSI — bit 22: 0=HSI
+                         | (4U   << 24);  // PLLQ = 4   — bits[27:24]
+
+	/* Enable PLL and wait for lock RCC->CR
+	 * bit 24 = PLLON
+	 * bit 25 = PLLRDY
+	*/
+	RCC->CR |= RCC_CR_PLLON;
+	while(!(RCC->CR & RCC_CR_PLLRDY));
+
+	/* Set AHB/APB prescalers (RCC_CFGR page 130, Sec 6.3.6)
+	 * AHB /1 = HCLK = 180 MHz (HPRE bits[7:4] -> 0000)
+	 * APB1 /4 = PCLK1 = 45 MHz (PPRE1 bits[12:10] -> 101)
+	 * APB2 /2 = PCLK2 = 90 MHz (PPRE2 bits[15:13] = 100)
+	*/
+	RCC->CFGR |= RCC_CFGR_PPRE1_DIV4 // apb1 PRESCALER
+			| RCC_CFGR_PPRE2_DIV2;	 // apb2 prescaler
+
+	/* Change the clock from SYCLK to PLL (RCC_CFGR - page 130, Sec 6.3.6)
+	 * 10: PLL used as the system clock
+	 * and confirm SWS
+	 */
+	RCC->CFGR |= RCC_CFGR_SW_PLL;
+	while((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
+
+}
+
+// SysTick init
+void systick_init(void)
+{
+	/* HCLK = 180 MHz, instead it will be HCLK/8 = 22.5 MHz
+	 * For 1 milliseconds = (2,25,00,000 / 1000) - 1 = 22499
+	 * SysTick is 24-bits, so 180 MHz = 1,67,77,215 which will fit
+	 * in 24 bits, but in this code it HCLK/8
+	 * Find the structure in core_cm4.h -> SysTick_Type
+	 * */
+	SysTick->LOAD = 22499U;	// set value
+	SysTick->VAL = 0U;		// clear current value
+
+	// CTRL: bit0=ENABLE, bit1=TICKINT (interrupt), bit2=CLKSRC (0=HCLK/8)
+	SysTick->CTRL = SysTick_CTRL_ENABLE_Msk | SysTick_CTRL_TICKINT_Msk;
+}
+
+/* Configure the button presS
+ * In STM32FR446E, B1 is wired to PC13 (schematic diagram), button pressed = 0
+ * The pin has an external pull-up in the board, no need to configure internal
+ * (Need to study)
+*/
+// Optional functionality
+//uint8_t button_pressed(void)
+//{
+//	/* IDR - Input Data Register (page 187, Sec 7.4.5)
+//	 * bitx = PCx -> bit13 = PC13
+//	 * Pressed=0, Released=1
+//	*/
+//	return !(GPIOC->IDR & (1U << 13));
+//}
+
+/* Debounce is important
+ * button switches bounces -> means they make or break the contact in the circuit
+ * if debounce is not addressed, for MCU it might look like we are doing 40 time in 20 ms
+ * */
+uint8_t button_debounce(void)
+{
+	// how to address it, add a delay, when the pin goes low
+	if(!(GPIOA->IDR & (1U << 13)))
+	{
+		delay_ms(20);
+		return !(GPIOA->IDR & (1U << 13));
+	}
+	return 0;
+}
 
 /**
   * @brief  The application entry point.
@@ -71,72 +141,42 @@ int main(void)
 {
 
   /* USER CODE BEGIN*/
-  /* Configure the clock */
+  /* Configure the clock
+   */
+  clock_init();
+  systick_init();
   /* Step 1: Enable clock */
-  /* Table 1. Page 55. GPIOA pin is mapped to AHB1 bus.
+  /* Table 1. Page 55. GPIOA, GPIOC pin is mapped to AHB1 bus.
    * Page 143. Sec 6.3.10 Enable peripheral clock RCC_AHB1ENR
-   * GPIOA in 0th bit
+   * GPIOA in 0th bit, GPIOC in 2th bit
   */
-  RCC->AHB1ENR |= (1U << 0); // setting 0th bit
-  /* Table 1. Page 59. USART2 is mapped to APB1 bus
-   * Page 145. Sec 6.3.13 Enable peripheral clock RCC_APB1ENR
-   * USART2EN in 17th bit
-  */
-  RCC->APB1ENR |= (1U << 17); // setting 17th bit
+  RCC->AHB1ENR |= (1U << 0) // setting 0th bit
+		  	   | (1U << 2);
 
-  /* Step 2: Configure GPIO pins & LED2 pin
-   * a. Find the pins for USART2 in the pinout diagram, PA2, PA3
-   * b. MODER is 2 bit pin, so 2n+1:2 -> PA2=[5:4], PA3=[7:6]
-   * c. Set PA2
-  */
-  // do not set the value directly, instead clear the field you want change
-  // and set the value
-  GPIOA->MODER &= ~(0x3U << 4); // USART2_TX
-  GPIOA->MODER |= (0x2U << 4);
 
-  /* d. Set the same for PA3 which is USART_RX
-  */
-  GPIOA->MODER &= ~(0x3U << 6);
-  GPIOA->MODER |= (0x2U << 6);
-
-  /* e. The Green LED will be connected to PA5 (schematic file)
+  /* Step 2: Configure LED2 pin
+   * a. Find the pins for LED2
+   * e. The Green LED will be connected to PA5 (schematic file)
    * PA5 ->[11:10]
    */
   GPIOA->MODER &= ~(0x3U << 10);
   GPIOA->MODER |= (0x1U << 10);
 
-  /* Step 3: Select AF7 for PA2 & PA3
-   * Table 11. Page.57 Data sheet PA2=AF7=USART2_TX, PA3=AF7=USART2_RX
-   * GPIOA_AFRL page 189. Section 7.4.9 AF7=0b0111=0x7
-   * AFRLy: Alternate function selection for port x bit y (y = 0..7)
-   * PA2 [11:8] & PA3 [15:12]
-  */
-  GPIOA->AFR[0] &= ~(0xFU << 8);	// clear PA2 [11:8]
-  GPIOA->AFR[0] |= (0x7U << 8); // PA2 = AF7
-  GPIOA->AFR[0] &= ~(0xFU << 12); // clear PA3 [15:12]
-  GPIOA->AFR[0] |= (0x7U << 12); // PA3 = AF7
-
-  /* Step 4: Configure USART
-   * BRR page 817 Section 25.6.3: 16 MHz HSI / 115200 baud = 138 = 0x8B
-   * */
-  USART2->BRR = 0x008B;
-  /* CR1 page 815 Section 25.6.4: bit13=UE, bit3=TE, bit2=RE
-  */
-  USART2->CR1 = (1U << 3) | (1U << 2) | (1U << 13);
-
-  /* Main loop
-   * IN BSRR page 188, bits[15:0] for set, bits[31:16] for clear
+  /* PC13 = input (button). Reset state of MODER is 00 = input.
+   * The Nucleo board has an external pull-up, so we leave PUPDR alone.
    */
-  while (1)
-  {
-	  GPIOA->BSRR = (1U << 5); // LED is ON
-	  uart_putchar('1');
-	  // volatile -> prevents compiler optimization, different from ATOMIC
-	  for(volatile uint32_t i =0; i < 500000 ; i++);// milliseconds
 
-	  GPIOA->BSRR = (1U << (5+16)); // LED is OFF
-	  uart_putchar('0');
-	  for(volatile uint32_t i =0; i < 500000 ; i++); // milliseconds
+  while(1)
+  {
+	  GPIOA->BSRR = (1U << 5);			// LED on
+	  delay_ms(500);
+	  GPIOA->BSRR = (1U << (5+16));		// LED off
+	  delay_ms(500);
+
+	  if (button_debounce())
+	  {
+		  delay_ms(100);
+	  }
   }
-}
 /* USER CODE END*/
+}
